8d99388d858103a8f3511ecea7b78dab48380993
Rikijaz <rikijaz34@gmail.com>
2019-03-24 23:49:22 -0700

    Phase 6 - Firing Shells
    Added the ability to fire projectiles, and made a UI & Sound fx to accompany the mechanic.

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -14,11 +14,11 @@ public class TankShooting : MonoBehaviour
     public float maxLaunchForce = 30f; 
     public float maxChargeTime = 0.75f;
 
-    /*
-    private string fireButton;         
-    private float currentLaunchForce;  
-    private float chargeSpeed;         
-    private bool fired;                
+
+    private string fireButton;
+    private float currentLaunchForce;
+    private float chargeSpeed;
+    private bool fired;
 
 
     private void OnEnable()
@@ -34,16 +34,52 @@ public class TankShooting : MonoBehaviour
 
         chargeSpeed = (maxLaunchForce - minLaunchForce) / maxChargeTime;
     }
-    */
 
+
+    // Track the current state of the fire button and make decisions based on the current launch force.
     private void Update()
     {
-        // Track the current state of the fire button and make decisions based on the current launch force.
+        aimSlider.value = minLaunchForce;
+
+        if ((currentLaunchForce >= maxLaunchForce) && (!fired))
+        {
+            currentLaunchForce = maxLaunchForce;
+            Fire();
+        }
+        else if (Input.GetButtonDown(fireButton))
+        {
+            fired = false;
+            currentLaunchForce = minLaunchForce;
+
+            shootingAudio.clip = chargingClip;
+            shootingAudio.Play();
+        }
+        else if (Input.GetButton(fireButton) && !fired)
+        {
+            currentLaunchForce += chargeSpeed * Time.deltaTime;
+            aimSlider.value = currentLaunchForce;
+        }
+        else if (Input.GetButtonUp(fireButton) && !fired)
+        {
+            Fire();
+        }
     }
 
-
+    // Instantiate and launch the shell.
     private void Fire()
     {
-        // Instantiate and launch the shell.
+        fired = true;
+
+        Rigidbody shellInstance = Instantiate(
+            shell,
+            fireTransform.position,
+            fireTransform.rotation) as Rigidbody;
+
+        shellInstance.velocity = currentLaunchForce * fireTransform.forward;
+
+        shootingAudio.clip = fireClip;
+        shootingAudio.Play();
+
+        currentLaunchForce = minLaunchForce;
     }
 }
